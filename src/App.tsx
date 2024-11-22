33bd4c3575149bf804598740cf7678434150c89d
陈骏鑫 <chenjunxin515024@163.com>
2024-11-22 18:10:50 +0800

    feat: 基础开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
-
 function App() {
+  const list = [
+    { label: '重要且紧急', type: '' },
+    { label: '重要不紧急', type: '' },
+    { label: '不重要且紧急', type: '' },
+    { label: '不重要不紧急', type: '' },
+  ]
   return (
     <>
-      app
+      <div className='grid cols-2 rows-2 relative'>
+        {list.map(i => (<div className='w-50vw h-50vh flex items-center justify-center text-13 border border-b-cyan border-solid'>{i.label}</div>))}
+      </div>
     </>
   )
 }
